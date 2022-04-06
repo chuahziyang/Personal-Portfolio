de397c7d0ada9035815872e52a3ac45210af57af
czyan <czyang2002@gmail,com>
2022-04-06 16:56:43 +0800

    port

---
+++ server/src/app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+console.log("Hello World");
+//express
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
