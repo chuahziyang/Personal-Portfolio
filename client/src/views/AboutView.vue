a6d83dfa08d2167ac55d5d781afba953e1da0e6d
czyan <czyang2002@gmail,com>
2022-04-13 13:05:16 +0800

    asd

--- client/src/views/AboutView.vue
+++ client/src/views/AboutView.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="about">
-    <h1 class="text-red-300">This is an about page</h1>
+  <div class="flex w-full h-screen relative bg-darkgray">
+    <a class="m-auto content-center text-white">
+      <h1>contact me</h1>
+    </a>
+    <a class="m-auto content-center text-white">github</a>
   </div>
 </template>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkgray: "#131313",
+      },
+    },
   },
   plugins: [],
 };
