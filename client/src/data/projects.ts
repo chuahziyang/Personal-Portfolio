ba6622e9894b7150b3ccd4c6e462c72657eda7db
czyan <czyang2002@gmail,com>
2022-05-07 20:37:09 +0800

    added stack

--- client/src/data/projects.ts
+++ client/src/data/projects.ts
@@ -3,6 +3,12 @@ interface project {
   linktoproject: string;
   linktoinfo: string;
   image: string;
+  stack: stack[];
+}
+
+interface stack {
+  stack: string;
+  colour: string;
 }
 
 const projects: project[] = [
@@ -11,78 +17,312 @@ const projects: project[] = [
     linktoproject: "https://mtrac.ternary.digital",
     linktoinfo: "/mtrac",
     image: "",
+    stack: [
+      {
+        stack: "React",
+        colour: "blue",
+      },
+      {
+        stack: "Vue.js",
+        colour: "green",
+      },
+      {
+        stack: "TailwindCSS",
+        colour: "blue",
+      },
+      {
+        stack: "Express",
+        colour: "gray",
+      },
+    ],
   },
   {
     name: "EMOB+",
     linktoproject: "https://emob.ternary.digital",
     linktoinfo: "/emob",
     image: "",
+    stack: [
+      {
+        stack: "React",
+        colour: "blue",
+      },
+      {
+        stack: "Vue.js",
+        colour: "green",
+      },
+      {
+        stack: "TailwindCSS",
+        colour: "blue",
+      },
+      {
+        stack: "Express",
+        colour: "gray",
+      },
+    ],
   },
   {
     name: "EMOB+",
     linktoproject: "https://emob.ternary.digital",
     linktoinfo: "/emob",
     image: "",
+    stack: [
+      {
+        stack: "React",
+        colour: "blue",
+      },
+      {
+        stack: "Vue.js",
+        colour: "green",
+      },
+      {
+        stack: "TailwindCSS",
+        colour: "blue",
+      },
+      {
+        stack: "Express",
+        colour: "gray",
+      },
+    ],
   },
   {
     name: "EMOB+",
     linktoproject: "https://emob.ternary.digital",
     linktoinfo: "/emob",
     image: "",
+    stack: [
+      {
+        stack: "React",
+        colour: "blue",
+      },
+      {
+        stack: "Vue.js",
+        colour: "green",
+      },
+      {
+        stack: "TailwindCSS",
+        colour: "blue",
+      },
+      {
+        stack: "Express",
+        colour: "gray",
+      },
+    ],
   },
   {
     name: "EMOB+",
     linktoproject: "https://emob.ternary.digital",
     linktoinfo: "/emob",
     image: "",
+    stack: [
+      {
+        stack: "React",
+        colour: "blue",
+      },
+      {
+        stack: "Vue.js",
+        colour: "green",
+      },
+      {
+        stack: "TailwindCSS",
+        colour: "blue",
+      },
+      {
+        stack: "Express",
+        colour: "gray",
+      },
+    ],
   },
   {
     name: "EMOB+",
     linktoproject: "https://emob.ternary.digital",
     linktoinfo: "/emob",
     image: "",
+    stack: [
+      {
+        stack: "React",
+        colour: "blue",
+      },
+      {
+        stack: "Vue.js",
+        colour: "green",
+      },
+      {
+        stack: "TailwindCSS",
+        colour: "blue",
+      },
+      {
+        stack: "Express",
+        colour: "gray",
+      },
+    ],
   },
   {
     name: "EMOB+",
     linktoproject: "https://emob.ternary.digital",
     linktoinfo: "/emob",
     image: "",
+    stack: [
+      {
+        stack: "React",
+        colour: "blue",
+      },
+      {
+        stack: "Vue.js",
+        colour: "green",
+      },
+      {
+        stack: "TailwindCSS",
+        colour: "blue",
+      },
+      {
+        stack: "Express",
+        colour: "gray",
+      },
+    ],
   },
   {
     name: "EMOB+",
     linktoproject: "https://emob.ternary.digital",
     linktoinfo: "/emob",
     image: "",
+    stack: [
+      {
+        stack: "React",
+        colour: "blue",
+      },
+      {
+        stack: "Vue.js",
+        colour: "green",
+      },
+      {
+        stack: "TailwindCSS",
+        colour: "blue",
+      },
+      {
+        stack: "Express",
+        colour: "gray",
+      },
+    ],
   },
   {
     name: "EMOB+",
     linktoproject: "https://emob.ternary.digital",
     linktoinfo: "/emob",
     image: "",
+    stack: [
+      {
+        stack: "React",
+        colour: "blue",
+      },
+      {
+        stack: "Vue.js",
+        colour: "green",
+      },
+      {
+        stack: "TailwindCSS",
+        colour: "blue",
+      },
+      {
+        stack: "Express",
+        colour: "gray",
+      },
+    ],
   },
   {
     name: "EMOB+",
     linktoproject: "https://emob.ternary.digital",
     linktoinfo: "/emob",
     image: "",
+    stack: [
+      {
+        stack: "React",
+        colour: "blue",
+      },
+      {
+        stack: "Vue.js",
+        colour: "green",
+      },
+      {
+        stack: "TailwindCSS",
+        colour: "blue",
+      },
+      {
+        stack: "Express",
+        colour: "gray",
+      },
+    ],
   },
   {
     name: "EMOB+",
     linktoproject: "https://emob.ternary.digital",
     linktoinfo: "/emob",
     image: "",
+    stack: [
+      {
+        stack: "React",
+        colour: "blue",
+      },
+      {
+        stack: "Vue.js",
+        colour: "green",
+      },
+      {
+        stack: "TailwindCSS",
+        colour: "blue",
+      },
+      {
+        stack: "Express",
+        colour: "gray",
+      },
+    ],
   },
   {
     name: "EMOB+",
     linktoproject: "https://emob.ternary.digital",
     linktoinfo: "/emob",
     image: "",
+    stack: [
+      {
+        stack: "React",
+        colour: "blue",
+      },
+      {
+        stack: "Vue.js",
+        colour: "green",
+      },
+      {
+        stack: "TailwindCSS",
+        colour: "blue",
+      },
+      {
+        stack: "Express",
+        colour: "gray",
+      },
+    ],
   },
   {
     name: "EMOB+",
     linktoproject: "https://emob.ternary.digital",
     linktoinfo: "/emob",
     image: "",
+    stack: [
+      {
+        stack: "React",
+        colour: "blue",
+      },
+      {
+        stack: "Vue.js",
+        colour: "green",
+      },
+      {
+        stack: "TailwindCSS",
+        colour: "blue",
+      },
+      {
+        stack: "Express",
+        colour: "gray",
+      },
+    ],
   },
 ];
 
