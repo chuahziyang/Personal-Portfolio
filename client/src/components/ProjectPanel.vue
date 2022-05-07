1cb3322072e91900c389251562ef83a33d46ad8e
czyan <czyang2002@gmail,com>
2022-05-07 15:26:24 +0800

    working link

--- client/src/components/ProjectPanel.vue
+++ client/src/components/ProjectPanel.vue
@@ -16,7 +16,13 @@ console.log(props);
   <div class="flex-col flex bg-red-800">
     <div class="bg-green-100 basis-3/5">{{ image }}</div>
     <div class="bg-blue-100 basis-2/5">
-      {{ name }}
+      <h5 class="text-3xl font-semibold text-gray-800">{{ name }}</h5>
+      <h6 class="text-2xl font-semibold text-gray-800">
+        Lorem ipsum dolor sit
+      </h6>
+      <a :href="linktoinfo" target="_">
+        <h6 class="text-2xl font-semibold text-blue-700">learn more</h6>
+      </a>
     </div>
   </div>
 </template>
