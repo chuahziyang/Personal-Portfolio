c2025cc5959064301153f37f5ab637c2170c1590
czyan <czyang2002@gmail,com>
2022-05-07 15:20:38 +0800

    dstructure project

--- client/src/components/ProjectPanel.vue
+++ client/src/components/ProjectPanel.vue
@@ -7,12 +7,16 @@ interface Props {
 }
 
 const props = defineProps<Props>();
+const { name, linktoinfo, linktoproject, image } = props.project;
+
 console.log(props);
 </script>
 
 <template>
   <div class="flex-col flex bg-red-800">
-    <div class="bg-green-100 basis-1/2">{{ props.project.name }}</div>
-    <div class="bg-blue-100 basis-1/2">{{ props.project.name }}</div>
+    <div class="bg-green-100 basis-3/5">{{ image }}</div>
+    <div class="bg-blue-100 basis-2/5">
+      {{ name }}
+    </div>
   </div>
 </template>
