21b28e6ce9cbe3d3825564c4c1abe57711733a6a
czyan <czyang2002@gmail,com>
2022-05-09 14:15:14 +0800

    working contact page

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import HomeView from "../views/HomeView.vue";
-import ProjectView from "../views/ProjectView.vue";
+import ProjectView from "../views/ContactView.vue";
 import EmobView from "../views/EmobView.vue";
 
 const routes: Array<RouteRecordRaw> = [
@@ -27,6 +27,15 @@ const routes: Array<RouteRecordRaw> = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+  {
+    path: "/contact",
+    name: "contact",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/ContactView.vue"),
   },
 ];
 
