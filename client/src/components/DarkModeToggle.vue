f10ea25afbb6d1d4d769d70e6056ab5cb9896e2d
chuahziyang <czyang2002@gmail.com>
2022-05-10 16:52:56 +0800

    working sun/moon

--- client/src/components/DarkModeToggle.vue
+++ client/src/components/DarkModeToggle.vue
@@ -1,5 +1,18 @@
+<script setup lang="ts">
+//eslint-disable-next-line
+const props = defineProps({
+  darkmode: {
+    type: Boolean,
+    default: false,
+  },
+});
+//define props
+</script>
+
 <template>
   <button>
-    <fa :icon="[`fas`, `moon`]" size="xl" target="_blank"> </fa>
+    <fa v-if="!darkmode" :icon="[`fas`, `moon`]" size="xl" target="_blank">
+    </fa>
+    <fa v-if="darkmode" :icon="[`fas`, `sun`]" size="xl" target="_blank"> </fa>
   </button>
 </template>
