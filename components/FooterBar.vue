b3c0d4868971fa46482c5b069e99e04ad732a9f2
chuahziyang <czyang2002@gmail.com>
2022-09-01 17:33:52 +0800

    added dark mode to footer

--- components/FooterBar.vue
+++ components/FooterBar.vue
@@ -8,7 +8,7 @@ const year = new Date().getFullYear();
   <div>
     <social-contacts></social-contacts>
     <div class="flex justify-center font-light">
-      <span class="pb-2 mt-4 text-xs font-light">
+      <span class="pb-2 mt-4 text-xs font-light dark:text-gray-200">
         {{ year }}
         <span>&#169;</span>
         Chuah Zi Yang
