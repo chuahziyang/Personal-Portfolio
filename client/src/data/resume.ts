292b8b88f38f79d55d68d7fd5a3cb106314c3073
chuahziyang <czyang2002@gmail.com>
2022-09-01 12:00:04 +0800

    working

--- client/src/data/resume.ts
+++
@@ -1,3 +0,0 @@
-const resumelink =
-  "https://docs.google.com/document/d/1Ag3GEtq8XgZViRhRKsf_nq1kGYsGDR9P/edit?usp=sharing&ouid=109120511381851187422&rtpof=true&sd=true";
-export default resumelink;
