0f863a7c30147f6d967a34a695b07b246761f7b1
chuahziyang <czyang2002@gmail.com>
2022-09-05 18:45:57 +0800

    made mobile view better

--- components/SideBar.vue
+++ components/SideBar.vue
@@ -1,17 +1,15 @@
 <template>
-  <header class="flex items-center justify-end py-8">
-    <div class="flex items-center text-base leading-5">
-      <div class="hidden sm:block">
-        <a
-          v-for="link in links"
-          :key="link"
-          :href="link.link"
-          class="p-1 font-medium text-gray-900 dark:text-gray-100 sm:p-4"
-        >
-          {{ link.name }}
-        </a>
-        <dark-mode-toggle />
-      </div>
+  <header class="flex items-center justify-center sm:justify-end py-8">
+    <div class="flex flex-row sm:block py-2">
+      <a
+        v-for="link in links"
+        :key="link"
+        :href="link.link"
+        class="font-medium text-gray-900 dark:text-gray-100 p-4"
+      >
+        {{ link.name }}
+      </a>
+      <dark-mode-toggle />
     </div>
   </header>
 </template>
--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,9 +1,17 @@
 <template>
   <view-wrapper>
+    <!-- <div class="bg-red-400 text-gray-100 font-medium mb-base">
+      <div
+        class="py-md px-lg md:py-md md:px-20 lg:py-md lg:px-20 xl:py-md xl:px-80"
+      >
+        You are on the staging server meant for testing. Data here is sandboxed
+        and does not reflect the production database.
+      </div>
+    </div> -->
     <h1>Projects</h1>
     <hr class="border-grey mb-3" />
     <div class="grow grid gap-x-8 gap-y-6 grid-cols-2">
-      <div class="border-2" v-for="project in projects" :key="project">
+      <div class="border-2" v-for="project in projects" :key="project.name">
         <project-panel class="h-80" :project="project"></project-panel>
       </div>
     </div>
