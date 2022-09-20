8ee2875cf423ee65e7eecdc3bbb69f704743f2ab
chuahziyang <czyang2002@gmail.com>
2022-09-20 16:15:33 +0800

    improved contact form

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,82 +1,58 @@
 <script setup lang="ts">
 import { projects } from "@/data/projects";
+import { Field, Form, ErrorMessage } from "vee-validate";
+
+function test(values) {
+  console.log(values);
+}
 </script>
 
 <template>
   <div>
     <top-banner></top-banner>
     <view-wrapper>
-      <h1>Contact</h1>
-      <hr class="border-grey mb-3" />
-      <h4>
-        Interested in contacting me for a project? Do feel free to drop me a
-        message!
-      </h4>
       <div class="mt-5 md:col-span-2 md:mt-0">
-        <form action="#" method="POST">
+        <Form @submit="test">
           <div class="overflow-hidden shadow sm:rounded-md">
-            <div class="bg-white px-4 py-5 sm:p-6">
+            <div class="bg-white dark:bg-thegray px-4 py-5 sm:p-6">
+              <h1>Contact Me</h1>
+              <hr class="border-grey mb-3" />
+              <h4 class="mb-8">
+                Interested in contacting me for a project? Do feel free to drop
+                me a message!
+              </h4>
               <div class="grid grid-cols-6 gap-6">
                 <div class="col-span-6 sm:col-span-3">
-                  <label
-                    for="first-name"
-                    class="block text-sm font-medium text-gray-700"
-                    >First name</label
-                  >
-                  <input
-                    type="text"
-                    name="first-name"
-                    id="first-name"
-                    autocomplete="given-name"
-                    class="mt-1 h-8 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                  />
+                  <label for="first-name" class="label">Name</label>
+                  <Field class="field" type="text" name="name" />
                 </div>
                 <div class="col-span-6 sm:col-span-3">
-                  <label
-                    for="last-name"
-                    class="block text-sm font-medium text-gray-700"
-                    >Last name</label
-                  >
-                  <input
-                    type="text"
-                    name="last-name"
-                    id="last-name"
-                    autocomplete="family-name"
-                    class="mt-1 h-8 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                  />
+                  <label for="company" class="label">Company *</label>
+                  <Field class="field" type="text" name="last-name" />
                 </div>
-                <div class="col-span-6 sm:col-span-4">
-                  <label
-                    for="email-address"
-                    class="block text-sm font-medium text-gray-700"
-                    >Email address</label
-                  >
-                  <input
-                    type="text"
-                    name="email-address"
-                    id="email-address"
-                    autocomplete="email"
-                    class="mt-1 h-8 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                  />
+                <div class="col-span-6">
+                  <label for="email-address" class="label">Email address</label>
+                  <Field class="field" type="text" name="email" />
                 </div>
-                <div class="col-span-6 sm:col-span-3">
-                  <label
-                    for="comment"
-                    class="block text-sm font-medium text-gray-700"
-                    >Add your comment</label
+                <div class="col-span-6">
+                  <label for="comment" class="label"
+                    >Anything you want to tell me?</label
                   >
                   <div class="mt-1">
-                    <textarea
+                    <Field
+                      as="textarea"
                       rows="4"
                       name="comment"
                       id="comment"
-                      class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                      class="dark:bg-gray-600 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
                     />
                   </div>
                 </div>
               </div>
             </div>
-            <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
+            <div
+              class="bg-gray-50 dark:bg-thegray px-4 py-3 text-right sm:px-6"
+            >
               <button
                 type="submit"
                 class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
@@ -85,7 +61,7 @@ import { projects } from "@/data/projects";
               </button>
             </div>
           </div>
-        </form>
+        </Form>
       </div>
     </view-wrapper>
   </div>
