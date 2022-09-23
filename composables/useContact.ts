3b4407e8376e6e5d2daf4d0b025730b8c5bfbcdc
chuahziyang <czyang2002@gmail.com>
2022-09-24 03:53:29 +0800

    working useContact

--- composables/useContact.ts
+++ composables/useContact.ts
@@ -8,24 +8,38 @@ interface Contact {
 }
 
 export const useContact = async (contact: Contact) => {
-  return true;
-  const runtimeConfig = useRuntimeConfig();
-  const { email, slurpApi } = runtimeConfig.public;
+  try {
+    const { content, name, company, email: senderemail } = contact;
 
-  console.log({ email, slurpApi });
-  const mailslurp = new MailSlurp({
-    apiKey: slurpApi,
-  });
-  // fetch emails
+    const runtimeConfig = useRuntimeConfig();
 
-  const inbox = await mailslurp.createInbox();
-  const options = {
-    to: [email],
-    subject: "asdasd",
-    body: contact.name,
-  };
+    const { email, slurpApi } = runtimeConfig.public;
 
-  const sent = await mailslurp.sendEmail(inbox.id, options);
+    console.log({ email, slurpApi });
+    const mailslurp = new MailSlurp({
+      apiKey: slurpApi,
+    });
+    // fetch emails
 
-  console.log(sent);
+    const inbox = await mailslurp.createInbox();
+    const options = {
+      to: [email],
+      subject: "New project",
+      body: `
+      name : ${name}
+      company : ${company}
+      email :${senderemail}
+      content :${content}
+      `,
+    };
+
+    const sent = await mailslurp.sendEmail(inbox.id, options);
+
+    console.log(sent);
+
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
 };
