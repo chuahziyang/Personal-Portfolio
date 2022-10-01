5f8626b357b9437ce2ec2a493f4b014e40167d4b
chuahziyang <czyang2002@gmail.com>
2022-10-01 11:41:43 +0800

    oops

--- components/ViewWrapper.vue
+++ components/ViewWrapper.vue
@@ -3,10 +3,8 @@
     <div
       class="sm:max-h-screen sm:max-w-screen flex h-full min-h-screen flex-col bg-white dark:bg-thegray justify-between"
     >
-      <side-bar class="bg-green-200"></side-bar>
-      <div
-        class="bg-orange-200 flex mx-10 sm:mx-auto grow max-w-3xl md:w-screen"
-      >
+      <side-bar class=""></side-bar>
+      <div class="flex mx-10 sm:mx-auto grow max-w-3xl md:w-screen">
         <slot></slot>
       </div>
       <footer-bar></footer-bar>
