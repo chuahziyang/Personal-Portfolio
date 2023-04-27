6a58ba08eb779e48e80eb3fceed8a7b05840b400
chuahziyang <czyang2002@gmail.com>
2023-04-27 15:23:15 +0800

    changed to submit

--- pages/contact.vue
+++ pages/contact.vue
@@ -89,7 +89,7 @@ function required(value) {
               type="submit"
               class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
             >
-              Save
+              Submit
             </button>
           </div>
         </div>
